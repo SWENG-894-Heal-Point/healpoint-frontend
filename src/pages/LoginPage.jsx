c783bb49b12c07062feca0a79ef6d4db755c0a5c
Mahfuzur Rahman <mvr5854@psu.edu>
2025-09-25 21:51:46 -0400

    Implement user authentication flow and add logout functionality

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,73 @@
-const LoginPage = (props) => {
-  return (
-    <>
-        <h1>Login Page</h1>
-    </>
-  );
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {Field, Formik} from "formik";
+import axios from "axios";
+import secureLocalStorage from "react-secure-storage";
+
+import AccountWrapper from "@/components/account/AccountWrapper";
+import style from "@/styles/account.module.css";
+
+
+const LoginPage = () => {
+    const [isShowPassword, setShowPassword] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const authToken = secureLocalStorage.getItem("auth-token");
+            if (authToken !== null) {
+                navigate("/");
+            }
+        }
+    }, []);
+
+    function handleSubmission(values) {
+        setErrorMessage("")
+
+        axios.post("/authenticate-user", JSON.stringify(values), {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    secureLocalStorage.setItem("auth-token", response.data);
+                    navigate("/");
+                }
+            })
+            .catch((err) => {
+                setErrorMessage(err.response.data);
+                console.log(err);
+            });
+    }
+
+    return (
+        <>
+            <Formik initialValues={{
+                email: '',
+                password: ''
+            }} onSubmit={(values, {setSubmitting}) => {
+                setTimeout(() => {
+                    setSubmitting(false);
+                }, 500);
+
+                handleSubmission(values);
+            }}>
+                <div className={style.login_form}>
+                    <AccountWrapper url="/signup" urlText="Create New Account" propmtText="">
+                        <Field id="email" name="email" placeholder="Email" type="email" required/>
+                        <Field id="password" name="password" placeholder="Password"
+                               type={isShowPassword ? "text" : "password"} required/>
+                        <span id={style.show_password}
+                              onClick={() => setShowPassword(!isShowPassword)}>{isShowPassword ? "Hide" : "Show"}</span>
+                        {errorMessage && <div className={style.error}>{errorMessage}</div>}
+                        <button type="submit">Login</button>
+                    </AccountWrapper>
+                </div>
+            </Formik>
+        </>
+    );
 };
 
 export default LoginPage;
