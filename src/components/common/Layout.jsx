ee183a855bba30bd27ff0584c8caeac08ea0dc74
Mahfuzur Rahman <mvr5854@psu.edu>
2025-09-27 10:14:06 -0400

    Develop common element and update style

---
+++ src/components/common/Layout.jsx
@@ -0,0 +1,44 @@
+import {useState} from "react";
+import {Link} from "react-router-dom";
+import {Sidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSquareCaretLeft, faSquareCaretRight} from "@fortawesome/free-solid-svg-icons";
+
+import style from "@/styles/layout.module.css";
+import LogoutButton from "@/components/account/LogOutButton.jsx";
+
+
+export default function Layout(props) {
+    const [collapsed, setCollapsed] = useState(false);
+
+    return (
+        <div className={style.layout}>
+            <div className={style.header}>
+                <h1>Heal Point</h1>
+            </div>
+            <div className={style.container}>
+                <Sidebar collapsed={collapsed} className={style.sidebar} backgroundColor="var(--navy-blue-bg-color)"
+                         width="220px">
+                    <Menu>
+                        <MenuItem className={style.collapsed_btn} onClick={() => setCollapsed(!collapsed)}>
+                            {collapsed ? <FontAwesomeIcon icon={faSquareCaretRight}/> :
+                                <FontAwesomeIcon icon={faSquareCaretLeft}/>}
+                        </MenuItem>
+                        {props.menuItems.map((item) => (
+                            <MenuItem className={location.pathname === item.path ? style.active_tab : ""}
+                                      key={item.path} component={<Link to={item.path}/>}>
+                                {item.label}
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                    <div className={style.sidebar_footer}>
+                        <LogoutButton/>
+                    </div>
+                </Sidebar>
+                <div className={style.main_content}>{props.children}</div>
+            </div>
+
+        </div>
+    );
+}
