bc10ec1de8c3971bf4e0022c3dc1fa33a2330b57
Mahfuzur Rahman <mvr5854@psu.edu>
2025-10-23 15:45:51 -0400

    Add interface to display a list of doctors

--- src/pages/DoctorDirectoryPage.jsx
+++ src/pages/DoctorDirectoryPage.jsx
@@ -1,16 +1,44 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import secureLocalStorage from "react-secure-storage";
 import axios from "axios";
 
 import Layout from "@/components/common/Layout.jsx";
 import DoctorProfileView from "@/components/account/DoctorProfileView.jsx";
 import SearchBar from "@/components/common/SearchBar.jsx";
+import {handleError} from "@/utils/handleError.js";
+import UserTable from "@/components/UserTable.jsx";
 
 
 export default function DoctorDirectoryPage() {
+    const [allDoctors, setAllDoctors] = useState([]);
     const [profileData, setProfileData] = useState(null);
-    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
     const authToken = secureLocalStorage.getItem("auth-token");
+
+    const columns = [
+        {field: 'firstName', headerName: 'First Name', flex: 1},
+        {field: 'lastName', headerName: 'Last Name', flex: 1},
+        {field: 'medicalDegree', headerName: 'Degree', flex: 1},
+        {field: 'specialty', headerName: 'Specialty', flex: 1},
+        {field: 'gender', headerName: 'Gender', flex: 1},
+    ];
+
+    useEffect(() => {
+        axios.get("/get-all-doctors", {
+            headers: {"Content-Type": "application/json"}
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log(response);
+                    setAllDoctors(response.data);
+                }
+            })
+            .catch((err) => {
+                handleError(err, setErrorMessage)
+                setProfileData(null);
+            });
+        // eslint-disable-next-line
+    }, []);
 
     function handleSearch(email) {
         const emailValue = typeof email === "object" && email.target ? email.target.value : email;
@@ -24,21 +52,28 @@ export default function DoctorDirectoryPage() {
                 }
             })
             .catch((err) => {
-                if (err.response) {
-                    setMessage(err.response.data);
-                    setProfileData(null);
-                }
-                console.error(err);
+                handleError(err, setErrorMessage)
+                setProfileData(null);
             });
     }
 
     return (
-        <>
-            <Layout>
-                <SearchBar handleSearch={handleSearch}/>
-                {profileData && <DoctorProfileView profileData={profileData}/>}
-                {!profileData && message && <p>{message}</p>}
-            </Layout>
-        </>
+        <Layout>
+            <SearchBar handleSearch={handleSearch}/>
+            {profileData ?
+                <>
+                    <DoctorProfileView profileData={profileData}/>
+                    <div className="default_btn_group">
+                        <button className="default_btn" onClick={() => setProfileData(null)}>
+                            Back
+                        </button>
+                    </div>
+                </>
+                :
+                allDoctors.length > 0 &&
+                <UserTable users={allDoctors} setProfileData={setProfileData} columns={columns}/>
+            }
+            {!profileData && errorMessage && <p>{errorMessage}</p>}
+        </Layout>
     );
 };
