144e8f82478f0e15392b0d0065f00754c32fd70c
Mahfuzur Rahman <mvr5854@psu.edu>
2025-11-14 11:10:53 -0500

    Add interface to display notifications

---
+++ src/components/dashboard/NotificationTable.jsx
@@ -0,0 +1,34 @@
+import {Paper} from "@mui/material";
+import {DataGrid} from "@mui/x-data-grid";
+import {defaultDataGridStyle} from "@/utils/defaultDataGridStyle.js";
+import style from "@/styles/user-list.module.css";
+
+export default function NotificationTable({notificationData}) {
+    const columns = [
+        {field: 'createdAt', headerName: 'Timestamp', flex: 1, minWidth: 120, maxWidth: 200},
+        {field: 'message', headerName: 'Message', flex: 3}
+    ];
+
+    const rows = notificationData.map(notification => ({
+        ...notification,
+        createdAt: new Date(notification.createdAt).toLocaleString()
+    }));
+
+    const dataGridStyle = defaultDataGridStyle();
+    const paginationModel = {page: 0, pageSize: 5};
+
+    return (
+        <Paper sx={{width: '100%', maxWidth: '900px'}}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{pagination: {paginationModel}}}
+                pageSizeOptions={[5, 10, 20, 50]}
+                getRowId={(row) => row.id}
+                disableRowSelectionOnClick
+                className={style.user_table}
+                sx={dataGridStyle}
+            />
+        </Paper>
+    );
+}
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,11 +1,39 @@
+import {useEffect, useState} from "react";
+import secureLocalStorage from "react-secure-storage";
+
+import DashboardTopSection from "@/components/dashboard/DashboardTopSection.jsx";
 import Layout from "@/components/common/Layout.jsx";
+import {useAppointmentList} from "@/utils/useAppointmentList.js";
+import axios from "axios";
+import {handleError} from "@/utils/handleError.js";
+import NotificationTable from "@/components/dashboard/NotificationTable.jsx";
 
 export default function DashboardPage() {
+    const [notificationData, setNotificationData] = useState([]);
+    const [_error, setError] = useState("");
+    const authToken = secureLocalStorage.getItem("auth-token");
+
+    const {_appointmentData, upcomingAppointments, _pastAppointments} = useAppointmentList(authToken, setError);
+
+    useEffect(() => {
+        axios.get("/get-my-notifications", {
+            params: {token: authToken}, headers: {"Content-Type": "application/json"}
+        }).then((response) => {
+            if (response.status === 200) {
+                setNotificationData(response.data);
+            }
+        }).catch((err) => {
+            handleError(err, setError)
+            setNotificationData([]);
+        });
+        // eslint-disable-next-line
+    }, []);
+
+
     return (
-        <>
-            <Layout>
-                <h2>Dashboard Page</h2>
-            </Layout>
-        </>
+        <Layout>
+            <DashboardTopSection upcomingAppointments={upcomingAppointments}/>
+            <NotificationTable notificationData={notificationData}/>
+        </Layout>
     );
 };
