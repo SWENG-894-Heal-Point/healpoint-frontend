bff9c42cab3fba097f74cf5c8609fece70df2704
Mahfuzur Rahman <mvr5854@psu.edu>
2025-11-19 20:51:19 -0500

    Add Material Icons and update search bar styling

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -1,11 +1,12 @@
 import {useState} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import {Search} from "@mui/icons-material";
 import style from "@/styles/searchbar.module.css";
 
 export default function SearchBar({handleEmailSearch, handleGenericSearch, enableClear, handleClear}) {
     const [searchValue, setSearchValue] = useState("");
     const emailRegex = /^[A-Za-z0-9.]+@[A-Za-z0-9]+\.[A-Za-z]{2,}$/;
+
+    const iconStyle = {verticalAlign: 'middle', width: 28, height: 28, };
 
     function handleSearch(searchValue) {
         const value = typeof searchValue === "object" && searchValue?.target ? searchValue.target.value : searchValue;
@@ -19,8 +20,10 @@ export default function SearchBar({handleEmailSearch, handleGenericSearch, enabl
     }
 
     function onSubmit(e) {
-        e.preventDefault(); // prevent page reload
-        handleSearch(searchValue);
+        e.preventDefault();
+        if (searchValue.trim() !== "") {
+            handleSearch(searchValue);
+        }
     }
 
     function onClear() {
@@ -31,6 +34,9 @@ export default function SearchBar({handleEmailSearch, handleGenericSearch, enabl
     return (
         <div className={style.search_container}>
             <form onSubmit={onSubmit} className={style.search_form}>
+                <button type="submit" className={style.search_button}>
+                    <Search sx={iconStyle}/>
+                </button>
                 <input
                     type="text"
                     className={style.search_input}
@@ -38,9 +44,6 @@ export default function SearchBar({handleEmailSearch, handleGenericSearch, enabl
                     value={searchValue}
                     onChange={(e) => setSearchValue(e.target.value)}
                 />
-                <button type="submit" className={style.search_button}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass}/>
-                </button>
             </form>
             {enableClear && <span className={style.clear_button} onClick={onClear}>Clear</span>}
         </div>
